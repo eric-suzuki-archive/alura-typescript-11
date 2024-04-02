a235dcc79513ba5045a6d811ba8b7ed075a27a3b
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-04-01 23:51:00 -0300

    Module 02-Aula 08

--- dist/js/views/mensagem-view.js
+++ dist/js/views/mensagem-view.js
@@ -1,14 +1,3 @@
-export class MensagemView {
-    constructor(seletor) {
-        this.element = document.querySelector(seletor);
-    }
-    template(model) {
-        return `
-            <p class="alert alert-info">${model}</p>
-        `;
-    }
-    update(model) {
-        const template = this.template(model);
-        this.element.innerHTML = template;
-    }
+import { View } from './view.js';
+export class MensagemView extends View {
 }
